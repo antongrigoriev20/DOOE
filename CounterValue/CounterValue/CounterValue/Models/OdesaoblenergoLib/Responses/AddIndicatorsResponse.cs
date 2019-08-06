d9ad025cf23a3c7b166dca26d15433329cd2c7aa
KostyaTymko <kostyatymko@gmail.com>
2019-08-06 08:32:10 +0300

    dited namespace of addindicators

--- CounterValue/CounterValue/CounterValue/Models/OdesaoblenergoLib/Responses/AddIndicatorsResponse.cs
+++ CounterValue/CounterValue/CounterValue/Models/OdesaoblenergoLib/Responses/AddIndicatorsResponse.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace DotNetConsoleTelegramBot.OdesaoblenergoLib.Responses
+namespace OdesaoblenergoLib.Models.Responses
 {
     public class AddIndicatorsResponse
     {
