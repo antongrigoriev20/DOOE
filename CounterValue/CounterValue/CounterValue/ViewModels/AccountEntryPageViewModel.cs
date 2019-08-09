b98c98ece2b3b9360afe78e71118b40f1031a7ed
KostyaTymko <kostyatymko@gmail.com>
2019-08-09 13:23:42 +0300

    added SelectIndicatorsResponse

--- CounterValue/CounterValue/CounterValue/ViewModels/AccountEntryPageViewModel.cs
+++ CounterValue/CounterValue/CounterValue/ViewModels/AccountEntryPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using CounterValue.Views;
+﻿using CounterValue.Models;
+using CounterValue.Views;
 using Newtonsoft.Json;
 using OdesaoblenergoLib.Models.Requests;
 using OdesaoblenergoLib.Models.Responses;
@@ -78,58 +79,84 @@ namespace CounterValue.ViewModels
             //если строка содержит "/"
             //if (Lic != null && Lic.Contains("/"))
             //{
-                //start progreessbar
-                try
+            //start progreessbar
+            try
+            {
+                HttpResponseMessage httpResponse;
+                AbonentInfoResponse AbonentInfo;
+                string response;
+
+                using (var client = new HttpClient())
                 {
-                    HttpResponseMessage httpResponse;
-                    AbonentInfoResponse AbonentInfo;
-                    string response;
+                    FormUrlEncodedContent content;
+                    string jsonValue;
+                    client.BaseAddress = new Uri(AppSettings.uri);
 
-                    using (var client = new HttpClient())
+                    #region получение токена
+                    jsonValue = JsonConvert.SerializeObject(new TokenRequestParams { Secret_key = AppSettings.secretKey, Name = AppSettings.name });
+                    content = new FormUrlEncodedContent(new[] { new KeyValuePair<string, string>(AppSettings.tokenMethod, jsonValue) });
+                    httpResponse = await client.PostAsync("/api/", content);
+                    httpResponse.EnsureSuccessStatusCode();
+                    response = await httpResponse.Content.ReadAsStringAsync();
+                    TokenResponse token = JsonConvert.DeserializeObject<TokenResponse>(response);
+                    #endregion
+
+                    #region получение адреса
+                    jsonValue = JsonConvert.SerializeObject(new AbonentInfoRequestParams { token = token.Response.Access_token, res = Lic.Substring(0, 2), abonent = "", lsch = Lic, proctype = "3", phoneno = "" });
+                    content = new FormUrlEncodedContent(new[] { new KeyValuePair<string, string>(AppSettings.infoMethod, jsonValue) });
+                    httpResponse = await client.PostAsync("/api/", content);
+                    httpResponse.EnsureSuccessStatusCode();
+                    response = await httpResponse.Content.ReadAsStringAsync();
+                    AbonentInfo = JsonConvert.DeserializeObject<AbonentInfoResponse>(response);
+                    #endregion
+
+                    //если ответ корректный
+                    if (!string.IsNullOrEmpty(AbonentInfo.response.lsch))
                     {
-                        FormUrlEncodedContent content;
-                        string jsonValue;
-                        client.BaseAddress = new Uri(AppSettings.uri);
-
                         #region получение токена
                         jsonValue = JsonConvert.SerializeObject(new TokenRequestParams { Secret_key = AppSettings.secretKey, Name = AppSettings.name });
                         content = new FormUrlEncodedContent(new[] { new KeyValuePair<string, string>(AppSettings.tokenMethod, jsonValue) });
                         httpResponse = await client.PostAsync("/api/", content);
                         httpResponse.EnsureSuccessStatusCode();
                         response = await httpResponse.Content.ReadAsStringAsync();
-                        TokenResponse token = JsonConvert.DeserializeObject<TokenResponse>(response);
+                        token = JsonConvert.DeserializeObject<TokenResponse>(response);
                         #endregion
 
-                        #region получение адреса
-                        jsonValue = JsonConvert.SerializeObject(new AbonentInfoRequestParams { token = token.Response.Access_token, res = Lic.Substring(0, 2), abonent = "", lsch = Lic, proctype = "3", phoneno = "" });
-                        content = new FormUrlEncodedContent(new[] { new KeyValuePair<string, string>(AppSettings.infoMethod, jsonValue) });
+                        #region получение данных счетчика
+                        jsonValue = JsonConvert.SerializeObject(new SelectIndicatorsRequestParams { token = token.Response.Access_token, abonent = AbonentInfo.response.AbonentId, res = Lic.Substring(0, 2) });
+                        content = new FormUrlEncodedContent(new[] { new KeyValuePair<string, string>(AppSettings.selectIndicatorsMethod, jsonValue) });
                         httpResponse = await client.PostAsync("/api/", content);
                         httpResponse.EnsureSuccessStatusCode();
                         response = await httpResponse.Content.ReadAsStringAsync();
-                        AbonentInfo = JsonConvert.DeserializeObject<AbonentInfoResponse>(response);
+                        SelectIndicatorsResponse SelectIndicatorsResponse = JsonConvert.DeserializeObject<SelectIndicatorsResponse>(response);
                         #endregion
 
-                        //если ответ корректный
-                        if (!string.IsNullOrEmpty(AbonentInfo.response.lsch))
-                        {
-                            //записываем данные по лицевому в памяти устройсва
-                            CrossSettings.Current.AddOrUpdateValue("lic", response);
+                        User User = new User {AbonentInfo=AbonentInfo,IndicatorsResponse=SelectIndicatorsResponse };
 
-                            await Application.Current.MainPage.DisplayAlert("Уведомление", $"Все ок: {AbonentInfo.response.lsch} {AbonentInfo.response.abonaddr}", "ОK");
-                            await Application.Current.MainPage.Navigation.PushModalAsync(new CounterValuePageView());
+                        if(Int32.Parse(SelectIndicatorsResponse.response.Zones) != 1)
+                        {
+                            await Application.Current.MainPage.DisplayAlert("Уведомление", $"Для этого лицевого счета используйте приложение Line Of Light", "ОK");
                             return;
                         }
-                        else
-                        {
-                            await Application.Current.MainPage.DisplayAlert("Уведомление", $"Cпробуйте знову", "ОK");
-                        }
+
+                        //записываем данные по лицевому в памяти устройсва
+                        CrossSettings.Current.AddOrUpdateValue("lic", Newtonsoft.Json.JsonConvert.SerializeObject(User));
+
+                        await Application.Current.MainPage.DisplayAlert("Уведомление", $"Все ок: {AbonentInfo.response.lsch} {AbonentInfo.response.abonaddr}", "ОK");
+                        await Application.Current.MainPage.Navigation.PushModalAsync(new CounterValuePageView());
+                        return;
+                    }
+                    else
+                    {
+                        await Application.Current.MainPage.DisplayAlert("Уведомление", $"Cпробуйте знову", "ОK");
                     }
                 }
+            }
 
-                catch
-                {
-                    await Application.Current.MainPage.DisplayAlert("Уведомление", $"Помилка, спробуйте знову", "ОK");
-                }
+            catch
+            {
+                await Application.Current.MainPage.DisplayAlert("Уведомление", $"Помилка, спробуйте знову", "ОK");
+            }
             //}
         }));
         #endregion
--- CounterValue/CounterValue/CounterValue/ViewModels/CounterValuePageViewModel.cs
+++ CounterValue/CounterValue/CounterValue/ViewModels/CounterValuePageViewModel.cs
@@ -1,10 +1,35 @@
-﻿using System;
+﻿using CounterValue.Models;
+using Newtonsoft.Json;
+using OdesaoblenergoLib.Models.Responses;
+using Plugin.Settings;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace CounterValue.ViewModels
 {
     class CounterValuePageViewModel
-    {
+    {      
+        public CounterValuePageViewModel()
+        {
+            string name = CrossSettings.Current.GetValueOrDefault("lic", null);
+            if (!string.IsNullOrEmpty(name))
+            {
+                User = JsonConvert.DeserializeObject<User>(name);
+                Lic = User.AbonentInfo.response.lsch;
+            }
+        }
+
+        User User;
+        public string Lic { get; set; }
+
+        private ICommand _sendIndicators;
+
+        public ICommand SendIndicators => _sendIndicators ?? (_sendIndicators = new Command(async () =>
+        {
+            //await Application.Current.MainPage.Navigation.PushAsync(new AccountEntryPageView());
+        }));
     }
 }
